7afd218beb6ca059df7c2e42bd7c746b67f495fb
Ryan Roseman <ryanroseman@yahoo.com>
2015-03-16 02:43:36 -0500

    ProductTemplateListView and ProductTemplateDetailView implemented and functioning

---
+++ src/assignment2/ProductTemplateDetailView.java
@@ -0,0 +1,177 @@
+package assignment2;
+
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class ProductTemplateDetailView extends JFrame {
+	private JPanel partFrame;
+	private JButton cancel, ok, edit, save;
+
+
+	private JLabel productID, productNumber, productDescription, errorMessage;
+	private JTextField idField, numberField, descriptionField;
+	private JComboBox<String> quantityUnitTypeField;	
+	private int viewWidth, viewHeight, errorW, errorH, buttonW, buttonH, buttonLeft, buttonBottom,
+				labelW, labelH, labelTop, labelLeft, fieldW, fieldH, fieldLeft, fieldTop;
+	
+	public ProductTemplateDetailView(ProductTemplateModel model, String title) {
+		super(title);
+		
+		viewWidth = 400;
+		viewHeight = 350;
+		labelW = viewWidth / 3;
+		labelH = 32;
+		labelTop = 15;
+		labelLeft = 15;
+		errorW = viewWidth - (labelLeft * 2);
+		errorH = 32;
+		buttonW = viewWidth / 4;
+		buttonH = 32;
+		buttonLeft = viewWidth / 3 - buttonW / 3;
+		buttonBottom = viewHeight - buttonH - 64;
+		fieldW = viewWidth / 2;
+		fieldH = 32;
+		fieldLeft = labelW + 25;
+		fieldTop = labelTop;
+		
+		this.setSize(viewWidth, viewHeight);
+		this.setVisible(true);
+		this.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width / 2) - (viewWidth / 2) + 50, 
+				 (Toolkit.getDefaultToolkit().getScreenSize().height / 2) - (viewHeight / 2));
+		
+		partFrame = new JPanel();
+		partFrame.setBackground(Color.LIGHT_GRAY);
+		partFrame.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(partFrame);
+		partFrame.setLayout(null);
+		
+		productID = new JLabel("ID");
+		productID.setBounds(labelLeft, labelTop + (labelH * 0), labelW, labelH);
+		partFrame.add(productID);
+		
+		productNumber = new JLabel("Product Number");
+		productNumber.setBounds(labelLeft, labelTop + (labelH * 1), labelW, labelH);
+		partFrame.add(productNumber);
+		
+		productDescription = new JLabel("Product Description");
+		productDescription.setBounds(labelLeft, labelTop + (labelH * 2), labelW, labelH);
+		partFrame.add(productDescription);
+		
+		errorMessage = new JLabel("");
+		errorMessage.setForeground(Color.red);
+		errorMessage.setBounds(labelLeft, labelTop + (labelH * 6), errorW, errorH);
+		partFrame.add(errorMessage);
+		
+		cancel = new JButton("Cancel");
+		cancel.setBounds(buttonLeft, buttonBottom, buttonW, buttonH);
+		partFrame.add(cancel);
+		
+		ok = new JButton("OK");
+		ok.setBounds(buttonLeft * 2, buttonBottom, buttonW, buttonH);
+		partFrame.add(ok);
+		
+		edit = new JButton("Edit");
+		edit.setBounds(buttonLeft * 2, buttonBottom, buttonW, buttonH);
+		partFrame.add(edit);
+		
+		save = new JButton("Save");
+		save.setBounds(buttonLeft * 2, buttonBottom, buttonW, buttonH);
+		partFrame.add(save);
+		
+		idField = new JTextField();
+		idField.setBounds(fieldLeft, fieldTop + (fieldH * 0), fieldW, fieldH);
+		partFrame.add(idField);
+		
+		numberField = new JTextField();
+		numberField.setBounds(fieldLeft, fieldTop + (fieldH * 1), fieldW, fieldH);
+		partFrame.add(numberField);
+		
+		descriptionField = new JTextField();
+		descriptionField.setBounds(fieldLeft, fieldTop + (fieldH * 2), fieldW, fieldH);
+		partFrame.add(descriptionField);
+		
+	}
+	
+	public void register(ProductTemplateListController controller) {
+		ok.addActionListener(controller);
+		cancel.addActionListener(controller);
+		edit.addActionListener(controller);
+		save.addActionListener(controller);
+		this.addWindowFocusListener(controller);
+	}
+	
+	public Integer getID() throws NumberFormatException {
+		Integer i = 0;
+		try {
+			i = Integer.parseInt(idField.getText().trim());
+			return i;
+		}
+		catch (NumberFormatException nfe) {
+			throw new NumberFormatException("Error: id must be in the form of an integer.");
+		}
+	}
+	public String getNumber() {
+		return numberField.getText();
+	}
+	
+    public String getDescription() {
+    	return descriptionField.getText();
+    }
+	
+	public void setErrorMessage(String error) {
+		errorMessage.setText(error);
+	}
+	
+	public void setID(Integer id) {
+		idField.setText(String.valueOf(id));
+	}
+	
+	public void setNumber(String number) {
+		numberField.setText(number);
+	}
+	
+	public void setDescription(String description) {
+		descriptionField.setText(description);
+	}
+	
+	public void hideID() {
+		idField.setVisible(false);
+		productID.setVisible(false);
+	}
+	
+	public void hideEditButton() {
+		edit.setVisible(false);
+	}
+	
+	public void hideSaveButton() {
+		save.setVisible(false);
+	}
+	
+	public void disableIDEdit() {
+		idField.setEnabled(false);
+	}
+	
+	public void disableEditable() {
+		ok.setVisible(false);
+		save.setVisible(false);
+		idField.setEnabled(false);
+		numberField.setEnabled(false);
+		descriptionField.setEnabled(false);
+	}
+	
+	public void enableEditable() {
+		save.setVisible(true);
+		numberField.setEnabled(true);
+		descriptionField.setEnabled(true);
+	}
+}
---
+++ src/assignment2/ProductTemplateListController.java
@@ -0,0 +1,179 @@
+package assignment2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class ProductTemplateListController implements ActionListener, ListSelectionListener, WindowFocusListener {
+	private ProductTemplateModel productTemplateModel;
+	private ProductTemplateListView productTemplateListView;
+	private ProductTemplateDetailView productTemplateDetailView;
+	private ProductTemplate selectedTemplate = null;
+	private int selectedRow = 0;
+	private boolean hasPartViewOpen;
+	
+	public ProductTemplateListController(ProductTemplateModel productTemplateModel, ProductTemplateListView productTemplateListView) {
+		this.productTemplateModel = productTemplateModel;
+		this.productTemplateListView = productTemplateListView;
+		hasPartViewOpen = false;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) throws NumberFormatException {
+		String command = e.getActionCommand();
+		
+		e.paramString();
+		
+		switch(command) {
+			case "Add": 
+				productTemplateListView.clearErrorMessage(); 
+				if (hasPartViewOpen) {
+					productTemplateDetailView.dispose();
+				}
+				clearSelection();
+				productTemplateDetailView = new ProductTemplateDetailView(productTemplateModel, "Add New Template");
+				productTemplateDetailView.register(this);
+				productTemplateDetailView.disableIDEdit();
+				productTemplateDetailView.hideSaveButton();
+				productTemplateDetailView.hideEditButton();
+				productTemplateDetailView.hideID();
+				hasPartViewOpen = true;
+				productTemplateListView.updatePanel();
+				productTemplateListView.repaint();
+				break;
+			case "Delete":
+				productTemplateListView.clearErrorMessage();
+				if (selectedTemplate != null) {
+					if (hasPartViewOpen) {
+						productTemplateDetailView.dispose();
+						hasPartViewOpen = false;
+					}
+					try {
+						productTemplateModel.deleteProductTemplate(selectedTemplate);
+						clearSelection();
+						productTemplateListView.updatePanel();
+						productTemplateListView.repaint();
+					} 
+					catch (SQLException sqe) {
+						productTemplateListView.setErrorMessage(sqe.getMessage());
+					}
+					catch (IOException ioe) {
+						productTemplateListView.setErrorMessage(ioe.getMessage());
+					}
+					
+				}
+				break;
+			case "View":
+				productTemplateListView.clearErrorMessage();
+				if (hasPartViewOpen) {
+					productTemplateDetailView.dispose();
+				}
+				if (selectedTemplate != null) {
+					productTemplateListView.disableDelete();
+					productTemplateListView.disableView();
+					productTemplateDetailView = new ProductTemplateDetailView(productTemplateModel, "View/Edit Template: " + selectedTemplate.getProductNumber());
+					productTemplateDetailView.register(this);
+					productTemplateDetailView.disableEditable();
+					productTemplateDetailView.setID(selectedTemplate.getID());
+					productTemplateDetailView.setNumber(selectedTemplate.getProductNumber());
+					productTemplateDetailView.setDescription(selectedTemplate.getDescription());
+					hasPartViewOpen = true;
+				}
+				break;
+			case "Edit":
+				productTemplateDetailView.enableEditable();
+				productTemplateDetailView.hideEditButton();
+				productTemplateDetailView.repaint();
+				break;
+			case "Save":
+				if (selectedTemplate != null) {
+					try {
+						ProductTemplate newTemplate = new ProductTemplate(productTemplateDetailView.getID(), productTemplateDetailView.getNumber(), productTemplateDetailView.getDescription());
+						productTemplateModel.editProductTemplate(selectedTemplate, newTemplate);
+						productTemplateDetailView.dispose();
+						productTemplateListView.updatePanel();
+						productTemplateListView.setSelectedRow(selectedRow);
+						productTemplateListView.repaint();
+					} catch (NumberFormatException noint) {
+						productTemplateDetailView.setErrorMessage(noint.getMessage());
+					} catch (SQLException sqe) {
+						productTemplateDetailView.setErrorMessage(sqe.getMessage());
+					} catch (IOException ex) {
+						productTemplateDetailView.setErrorMessage(ex.getMessage());
+					} catch (Exception e1) {
+						productTemplateDetailView.setErrorMessage(e1.getMessage());
+					} 				
+				}	
+				break;
+			case "OK":
+				try {
+					ProductTemplate newTemplate = new ProductTemplate(productTemplateDetailView.getID(), productTemplateDetailView.getNumber(), productTemplateDetailView.getDescription());			
+					productTemplateModel.addProductTemplate(newTemplate);
+					productTemplateDetailView.dispose();
+					productTemplateListView.updatePanel();
+					productTemplateListView.repaint();
+				}
+				catch (NumberFormatException noint) {
+					productTemplateDetailView.setErrorMessage(noint.getMessage());
+				}
+				catch (IOException ioex) {
+					productTemplateDetailView.setErrorMessage(ioex.getMessage());
+				}
+				catch (Exception ex) {
+					productTemplateDetailView.setErrorMessage(ex.getMessage());
+				}
+				break;
+			case "Cancel":
+				productTemplateListView.enableDelete();
+				productTemplateListView.enableView();
+				productTemplateDetailView.dispose();
+				break;
+		}
+	}
+	
+	private void clearSelection() {
+		selectedTemplate = null;
+		productTemplateListView.disableDelete();
+		productTemplateListView.disableView();
+	}
+	
+	// When the user clicks on an element in the inventory list, event is triggered: gets Part from index of list element
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+        if (e.getValueIsAdjusting()) return;
+		ListSelectionModel lsm = (ListSelectionModel) e.getSource();
+		if (lsm.isSelectionEmpty()) {
+			return;
+		}
+		else {
+			selectedRow = lsm.getMinSelectionIndex();	
+			selectedTemplate = productTemplateListView.getObjectInRow(selectedRow);
+			productTemplateListView.clearErrorMessage();
+			productTemplateListView.enableDelete();
+			productTemplateListView.enableView();
+		}
+	}
+	
+	@Override
+	public void windowGainedFocus(WindowEvent e) {
+		if (selectedTemplate != null) {
+			productTemplateListView.updatePanel();
+			productTemplateListView.setSelectedRow(selectedRow);
+		}	
+	}
+
+	@Override
+	public void windowLostFocus(WindowEvent e) {
+		if (selectedTemplate != null) {
+			productTemplateListView.enableDelete();
+			productTemplateListView.enableView();
+		}
+	}
+}
